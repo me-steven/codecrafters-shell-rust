8bcbe08c1d7f39c934458f3c42aae8554d457d84
STVND <42823678+me-steven@users.noreply.github.com>
2026-05-22 21:59:50 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,25 @@ fn command_echo(c: String) {
     print!("{}", &c[5..]);
 }
 
+fn command_type(c: String) {
+    let given: &str = &c[5..].trim();
+
+    match given {
+        "type" => println!("{} is a shell builtin", given),
+        "echo" => println!("{} is a shell builtin", given),
+        "exit" => println!("{} is a shell builtin", given), 
+        _ => println!("{}: not found", given.trim())
+    }
+}
+
 fn main() {
     loop {
         let mut command = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        match command.as_str().split_whitespace().next().unwrap_or("") {
+        match command.as_str().split_whitespace().next().unwrap_or("").trim() {
+            "type" => command_type(command),
             "echo" => command_echo(command),
             "exit" => break,
             _ => command_does_not_exists(command)
