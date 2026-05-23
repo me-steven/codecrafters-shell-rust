7e165c1fcb53eb7c8a3141b1deddb674698f97c8
STVND <42823678+me-steven@users.noreply.github.com>
2026-05-22 21:50:29 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn command_not_exists(c: String) {
+    println!("{}: command not found", c.trim());
+}
 fn main() {
     loop {
         let mut command = String::new();
@@ -10,7 +13,8 @@ fn main() {
         match command.as_str().split_whitespace().next().unwrap_or("") {
             "echo" => print!("{}", &command[5..]),
             "exit" => break,
-            _ => println!("{}: command not found", command.trim())
+            // _ => println!("{}: command not found", command.trim())
+            _ => command_not_exists(command)
         }
     }
 }
