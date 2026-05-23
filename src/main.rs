a4e735fca64bce166150e7f2c6fcd903ce43a424
STVND <42823678+me-steven@users.noreply.github.com>
2026-05-22 22:33:16 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn command_does_not_exists(c: String) {
-    println!("{}: command not found", c.trim());
+fn command_not_built_in(c: &str) {
+    if which::which(c.trim()).is_ok() {
+        println!("{}: is a shell builtin", c.trim());
+    } else {
+        println!("{}: command not found", c.trim());
+    }
+    
 }
 
 fn command_echo(c: String) {
@@ -26,11 +31,12 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
+        
         match command.as_str().split_whitespace().next().unwrap_or("").trim() {
             "type" => command_type(command),
             "echo" => command_echo(command),
             "exit" => break,
-            _ => command_does_not_exists(command)
+            _ => command_not_built_in(command.trim())
         }
     }
 }
