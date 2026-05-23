3b6dbceeafd96d9a0c2a4ed0f230c7fb97f7136c
STVND <42823678+me-steven@users.noreply.github.com>
2026-05-22 21:29:52 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        match command {
+            _ => println!("{}: command not found", command.trim())
+        }
     }
 }
