a3801d457ecd9bde9c82b993ab4efb177fb8fa9e
STVND <42823678+me-steven@users.noreply.github.com>
2026-05-23 20:35:35 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,7 @@ fn command_not_found(args: &mut dyn Iterator<Item = &str>) {
     };
  
     Command::new(&full_path)
+    .arg(c)
     .args(args)
     .status()
     .expect("Failed to execute command");
